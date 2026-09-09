6b5b33d9088588ee7cbd9b9a485f1c5ceeb3c5e5
dnjor <mstrldyfy@gmail.com>
2026-09-09 04:50:08 +0300

    Learn the basic fundamentals of Kotlin language

--- frontend/app/src/main/java/com/aidocumentationassistant/app/MainActivity.kt
+++ frontend/app/src/main/java/com/aidocumentationassistant/app/MainActivity.kt
@@ -3,45 +3,64 @@ package com.aidocumentationassistant.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import com.aidocumentationassistant.app.ui.theme.FrontendTheme
 
+
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             FrontendTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "me",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MyInput()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    FrontendTheme {
-        Greeting("Android")
+fun MyInput() {
+
+    var name by remember {
+        mutableStateOf("")
+    }
+
+    var greeting by remember {
+        mutableStateOf("")
+    }
+
+    Column {
+        TextField(
+            value = name,
+            onValueChange = { newValue ->
+                name = newValue
+            },
+            label = {
+                Text("Enter your name")
+            }
+        )
+
+        Button(
+            onClick = {
+                greeting = "hello $name"
+            }
+        ) {
+            Text("Submit")
+        }
+
+        Text(
+            text = greeting
+        )
     }
 }
